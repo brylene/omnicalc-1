0390448b564c92832a3b98b6cd572554f60262f3
brylenelavelle Patrick <brylenelavelle@gmail.com>
2023-03-23 17:18:54 +0000

    unfin payment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 class ApplicationController < ActionController::Base
   def blank_square_form
-  
-      render({ :template => "calculation_templates/square_form.html.erb"})
+    render({ :template => "calculation_templates/square_form.html.erb" })
   end
 
   def calculate_square
     @num = params.fetch("number").to_f
     @square_of_num = @num ** 2
-    render({ :template => "calculation_templates/square_results.html.erb"})
+    render({ :template => "calculation_templates/square_results.html.erb" })
   end
 
   def calculate_random
@@ -19,19 +18,30 @@ class ApplicationController < ActionController::Base
   end
 
   def blank_rand_form
-  
-    render({ :template => "calculation_templates/rand_form.html.erb"})
+    render({ :template => "calculation_templates/rand_form.html.erb" })
   end
 
   def blank_root_form
-
-    render({ :template => "calculation_templates/root_form.html.erb"})
+    render({ :template => "calculation_templates/root_form.html.erb" })
   end
 
   def calculate_root
-    @root = params.fetch("user_number").to_i
+    @root = params.fetch("user_number").to_f
     @square_root_num = Math.sqrt(@root).to_f
 
-    render({ :template => "calculation_templates/root_results.html.erb"})
+    render({ :template => "calculation_templates/root_results.html.erb" })
+  end
+
+  def blank_payment_form
+    render({ :template => "calculation_templates/payment_form.html.erb" })
+  end
+
+  def calculate_payment
+    @apr = params.fetch("user_apr").to_f
+    @years = params.fetch("user_years").to_i
+    @principal = params.fetch("user_pv").to_f
+    @payment = ((@apr / @years) * @principal)
+
+    render({ :template => "calculation_templates/payment_results.html.erb" })
   end
 end
